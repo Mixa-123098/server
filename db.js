62e12def662b0212d1ce7eb01ed06cc9d840bd66
Mixa-123098 <60194946+Mixa-123098@users.noreply.github.com>
2024-03-19 17:54:00 +0200

    .

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  password: "123098",
+  user: "portfolio_zdxw_user",
+  password: "VpIC48TmECcU8fyfgyCKxw1qLkcMRa7G",
   // dlit
-  host: "localhost",
+  host: "dpg-cnsok20l6cac73dbjavg-a.frankfurt-postgres.render.com",
   port: 5432,
-  database: "projects",
+  database: "portfolio_zdxw",
 });
 
 module.exports = pool();
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "123098",
+//   // dlit
+//   host: "localhost",
+//   port: 5432,
+//   database: "projects",
+// });
+
+// module.exports = pool();
