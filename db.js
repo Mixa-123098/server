4983556f6a420a606cc0f832ce989fc4761d8fb8
Mixa-123098 <60194946+Mixa-123098@users.noreply.github.com>
2024-07-03 13:25:41 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "portfolio_zdxw_user",
+//   password: "VpIC48TmECcU8fyfgyCKxw1qLkcMRa7G",
+//   // dlit
+//   host: "dpg-cnsok20l6cac73dbjavg-a.frankfurt-postgres.render.com",
+//   port: 5432,
+//   database: "portfolio_zdxw",
+// });
+
 const pool = new Pool({
-  user: "portfolio_zdxw_user",
-  password: "VpIC48TmECcU8fyfgyCKxw1qLkcMRa7G",
+  user: "site_portfolio_final_version_user",
+  password: "AomyENvMp6nTOtzZfdpVRKjY2uKSS8MF",
   // dlit
-  host: "dpg-cnsok20l6cac73dbjavg-a.frankfurt-postgres.render.com",
+  host: "dpg-cq2i1tbv2p9s73etb0n0-a.oregon-postgres.render.com",
   port: 5432,
-  database: "portfolio_zdxw",
+  database: "site_portfolio_final_version",
 });
 
 module.exports = pool();
